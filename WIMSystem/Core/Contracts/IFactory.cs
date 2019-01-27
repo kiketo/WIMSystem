7ff51e3bb8a43b30b003cc94455116024aa581cb
Kiril Kuzmanov <kiketo@gmail.com>
2019-01-27 15:07:03 +0200

    Added&implemented IFactory&Singleton

---
+++ WIMSystem/Core/Contracts/IFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+using WIMSystem.Models;
+using WIMSystem.Models.Contracts;
+using WIMSystem.Models.Abstract;
+using WIMSystem.Models.Enums;
+
+namespace WIMSystem.Core.Contracts
+{
+    interface IFactory
+    {
+         
+        Board CreateBoard (string name, List <WorkItem> workItems,List <HistoryItem>historyItems);
+
+        Bug CreateBug(string title, string description, IList<string> stepsToReproduce,
+            PriorityType priority, BugSeverityType severity, BugStatusType bugStatus,
+            IMember assignee = null);
+
+        Comment CreateComment(string message, Member author);
+
+        Feedback CreateFeedback(int rating, FeedbackStatusType feedbackStatusType);
+
+        HistoryItem CreateHistoryItem(string description, DateTime creationDate, IMember member, IBoard board, ITeam team);
+
+        Member CreateMember(string memberName, IList<IWorkItem> memberWorkItems);
+
+        //Story CreateStory();
+
+        Team CreateTeam(string name, List<Member>membersList, List<Board>boardsList, List<HistoryItem>historyList);
+
+    }
+}
---
+++ WIMSystem/Core/Engine/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WIMSystem.Models;
+using WIMSystem.Models.Contracts;
+
+
+namespace WIMSystem.Core.Engine
+{
+    public class WIMTeams : IWIMTeams, IEnumerable<ITeam>
+    {
+        // The single instance 
+        private static List<Team> teamList;
+
+        // Initialize the single instance 
+        static WIMTeams()
+        {
+            teamList = new List<Team>();
+        }
+
+        // Private constructor – protects against direct instantiation 
+        private WIMTeams() { }
+
+        // The property for retrieving the single instance 
+        public List<Team> TeamList
+        {
+            get {
+                
+                return teamList; }
+            private set { this.teamList = value; }
+        }
+
+
+        public void AddTeam(Team newTeam)
+        {
+          this.TeamList.
+        }
+
+        //Надолу дали е ОК??? и какво става с другия WINTeams.cs ?
+
+
+       // public IList<ITeam> TeamsList
+       // {
+       //     get => new List<ITeam>(teamList);
+       // }
+
+        public IEnumerator<ITeam> GetEnumerator()
+        {
+            foreach (var item in this.teamList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
