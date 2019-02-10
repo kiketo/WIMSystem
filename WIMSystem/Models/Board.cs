6668c966811ab5aadccdc122abb3e8de0a64a512
Stakata <stanislav.p.dinev@gmail.com>
2019-02-10 16:48:38 +0200

    bug fixed

--- WIMSystem/Models/Board.cs
+++ WIMSystem/Models/Board.cs
@@ -114,7 +114,7 @@ namespace WIMSystem.Models
             }
             if (!string.IsNullOrEmpty(sortBy))
             {
-                var a = this.BoardWorkItems.First().Value.GetType().GetProperty(sortBy);
+                var a = filteredCollection.First().GetType().GetProperty(sortBy);
                 filteredCollection = filteredCollection.OrderBy(x => a.GetValue(x, null));
             }
             StringBuilder result = new StringBuilder();
