2999d2d143c4091b9ce85dbe173a7ecc99e4b89a
Stakata <stanislav.p.dinev@gmail.com>
2019-02-13 15:32:36 +0200

    Commands skeleton and interface added

---
+++ WIMSystem/Commands/Contracts/IEngineCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIMSystem.Commands.Contracts
+{
+    public interface IEngineCommand
+    {
+        string ReadSingleCommand();
+    }
+}
