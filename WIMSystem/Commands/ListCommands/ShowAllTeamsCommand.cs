223f523ff2cc4c5e93b8d8be82173458b8ec55ee
Kiril Kuzmanov <kiketo@gmail.com>
2019-02-15 11:08:16 +0200

    ShowAllTeamsCommand Implemented

---
+++ WIMSystem/Commands/ListCommands/ShowAllTeamsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Utils;
+using WIMSystem.Commands.Contracts;
+using WIMSystem.Models;
+using WIMSystem.Models.Contracts;
+
+namespace WIMSystem.Commands.ListCommands
+{
+    public class ShowAllTeamsCommand : IEngineCommand
+    {
+        private IWIMTeams wIMTeams;
+
+        public ShowAllTeamsCommand(IWIMTeams wIMTeams)
+        {
+            this.wIMTeams = wIMTeams ?? throw new ArgumentNullException(nameof(wIMTeams));
+        }
+
+        public string ReadSingleCommand(IList<string> parameters)
+        {            
+            return this.Execute();
+        }
+
+        private string Execute()
+        {
+            var returnMessage = this.wIMTeams.ShowAllTeams();
+            return returnMessage;
+        }
+    }
+}
