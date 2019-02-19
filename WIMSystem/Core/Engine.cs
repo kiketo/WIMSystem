f659cf4ff8f03ee317036aa10672f790ebc6ca5c
Stiliyan Kostadinov <st.k.kostadinov@gmail.com>
2019-02-19 14:42:51 +0200

    Added text to the "ShowAllTeams" command

--- WIMSystem/Core/Engine.cs
+++ WIMSystem/Core/Engine.cs
@@ -82,10 +82,10 @@ namespace WIMSystem.Core
                         this.printReports.Reports.Add(report);
                     }
                 }
-                catch (ArgumentOutOfRangeException)
-                {
-                    this.printReports.Reports.Add("Invalid..."); //TODO
-                }
+                //catch (ArgumentOutOfRangeException)
+                //{
+                //    this.printReports.Reports.Add("Invalid...");
+                //}
                 catch (Exception ex)
                 {
                     this.printReports.Reports.Add(ex.Message);
@@ -93,6 +93,5 @@ namespace WIMSystem.Core
                 }
             }
         }
-
     }
 }
