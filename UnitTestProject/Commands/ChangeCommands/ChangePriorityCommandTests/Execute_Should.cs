374be387b57506debd1a5dc937b3d2f143ce852d
Stiliyan Kostadinov <st.k.kostadinov@gmail.com>
2019-02-23 01:14:47 +0200

    Added another Test Method for ChangePriority Command

--- UnitTestProject/Commands/ChangeCommands/ChangePriorityCommandTests/Execute_Should.cs
+++ UnitTestProject/Commands/ChangeCommands/ChangePriorityCommandTests/Execute_Should.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using WIMSystem.Commands.ChangeCommands;
 using WIMSystem.Commands.Contracts;
+using WIMSystem.Commands.Utils;
 using WIMSystem.Models.Contracts;
 using WIMSystem.Models.Enums;
 
@@ -27,33 +28,79 @@ namespace WIMSystem.Tests.Commands.ChangeCommands.ChangePriorityCommandTests
             var workItemMock = new Mock<IBug>();
             var mockAssignee = new Mock<IPerson>();
             var teamMock = new Mock<ITeam>();
-            var workItemCollection = new Dictionary<string, IWorkItem>();
-
-
-
-            workItemCollection.Add(validWorkItemTitle, workItemMock.Object);
 
             gettersMock.Setup(b => b.GetBoard(validTeamName, validBoardName)).Returns(boardMock.Object);
             gettersMock.Setup(w => w.GetAssignableWorkItem(boardMock.Object, validWorkItemTitle)).Returns(workItemMock.Object);
 
             boardMock.Setup(n => n.BoardName).Returns(validBoardName);
             boardMock.Setup(n => n.Team).Returns(It.IsAny<ITeam>());
-            boardMock.Setup(n => n.BoardWorkItems).Returns(workItemCollection);
 
             workItemMock.Setup(p => p.Priority).Returns(PriorityType.Low);
             workItemMock.Setup(p => p.Title).Returns(validWorkItemTitle);
             workItemMock.Setup(a => a.Assignee).Returns(mockAssignee.Object);
             workItemMock.Setup(a => a.Board).Returns(boardMock.Object);
             workItemMock.Setup(a => a.Board.Team).Returns(teamMock.Object);
-            
+
             var parameters = new List<string>() { validTeamName, validBoardName, validWorkItemTitle, priority };
 
             var sut = new ChangePriorityCommand(historyEventWriterMock.Object, gettersMock.Object);
 
-            sut.Execute(parameters);
+            var returnMessage = sut.Execute(parameters);
 
             gettersMock.Verify(x => x.GetBoard(validTeamName, validBoardName), Times.Once);
-
+            gettersMock.Verify(x => x.GetAssignableWorkItem(boardMock.Object, validWorkItemTitle), Times.Once);
+            historyEventWriterMock.
+            Verify(x => x.AddHistoryEvent(
+                returnMessage,
+                mockAssignee.Object,
+                It.IsAny<IBoard>(),
+                It.IsAny<ITeam>(),
+                workItemMock.Object
+                ), Times.Once);
         }
+
+        [TestMethod]
+        public void ReturnSuccessMessage_WhenValidDataIsPassed()
+        {
+            var validTeamName = "validTeam";
+            var validBoardName = "validB";
+            var validWorkItemTitle = "validW";
+            var priority = "High";
+
+            var historyEventWriterMock = new Mock<IHistoryEventWriter>();
+            var gettersMock = new Mock<IGetters>();
+            var boardMock = new Mock<IBoard>();
+            var workItemMock = new Mock<IBug>();
+            var mockAssignee = new Mock<IPerson>();
+            var teamMock = new Mock<ITeam>();
+
+            gettersMock.Setup(b => b.GetBoard(validTeamName, validBoardName)).Returns(boardMock.Object);
+            gettersMock.Setup(w => w.GetAssignableWorkItem(boardMock.Object, validWorkItemTitle)).Returns(workItemMock.Object);
+
+            boardMock.Setup(n => n.BoardName).Returns(validBoardName);
+            boardMock.Setup(n => n.Team).Returns(It.IsAny<ITeam>());
+
+            workItemMock.Setup(p => p.Priority).Returns(PriorityType.Low);
+            workItemMock.Setup(p => p.Title).Returns(validWorkItemTitle);
+            workItemMock.Setup(a => a.Assignee).Returns(mockAssignee.Object);
+            workItemMock.Setup(a => a.Board).Returns(boardMock.Object);
+            workItemMock.Setup(a => a.Board.Team).Returns(teamMock.Object);
+
+            var parameters = new List<string>() { validTeamName, validBoardName, validWorkItemTitle, priority };
+
+            var sut = new ChangePriorityCommand(historyEventWriterMock.Object, gettersMock.Object);
+
+            var actualMessage = sut.Execute(parameters);
+
+            var expecterMessage = string.Format(CommandsConsts.WorkItemPriorityChange, workItemMock.Object.Title, priority);
+
+            Assert.AreEqual(expecterMessage, actualMessage);
+        }
+
+        //TODO: Member is assignee if it is assignable work item
+
+        //TODO: Member is null if it is not assignable work item
+
+        //TODO: ExceptionThrowing
     }
 }
