db05e29733490144267a97cb39c146a012729c22
Kiril Kuzmanov <kiketo@gmail.com>
2019-02-23 20:27:58 +0200

    ChangeRatingOfFeedbackCommandTests started

---
+++ UnitTestProject/Commands/ChangeCommands/ChangeRatingOfFeedbackCommandTests/Execute_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIMSystem.Tests.Commands.ChangeCommands.ChangeRatingOfFeedbackCommandTests
+{
+    class Execute_Should
+    {
+    }
+}
